e55cf3964302e970ae713c944eebf549345b16db
JonPaulDon <79511635+JonPaulDon@users.noreply.github.com>
2022-04-18 21:49:32 -0400

    Version 3
    Started on login functionality
    Associated a 2d array with our grid

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 /* References: 
-https://javascript.plainenglish.io/the-game-of-life-using-javascript-fc1aaec8274f
+This was referenced for building the grid: https://javascript.plainenglish.io/the-game-of-life-using-javascript-fc1aaec8274f
+
 */
 
 //user input grid size
+var gameArray;
+var inactive_array = [];
 var size = window.prompt("Enter Number for Grid Size: ");
-
+createArray();
 //function that loads the game
-function startgame() {
+function startgame(){
     var display = document.getElementById("grid");
     var grid = creategrid();
     display.appendChild(grid);  
@@ -21,7 +24,7 @@ function creategrid() {
         var row = gridrows(tr);
         for (var j = 0; j < size; j++) {
             var td = "td_" + i + "_" + j;
-            var col = gridcolumns(td);
+            var col = gridcolumns(td,i,j);
             row.appendChild(col);
         }
         grid.appendChild(row);
@@ -35,11 +38,25 @@ function gridrows(name) {
     return rowattribute;
 }
 
-function gridcolumns(name) {
+function gridcolumns(name,row,column) {
     var colattribute = document.createElement("td");
     colattribute.setAttribute("id", name);
-    colattribute.setAttribute("onclick", "clickcells(this)")
+    colattribute.setAttribute("onclick", "select(this)");
+    colattribute.setAttribute("row", row);
+    colattribute.setAttribute("col", column);
     return colattribute;
+}
+
+function select(td){
+  let row=td.getAttribute('row');
+  let col=td.getAttribute('col');
+  if(gameArray[row][col]==0){
+    gameArray[row][col]++;
+  }else {
+    gameArray[row][col]--;
+  }
+  //alert(`You clicked row ${row} and col ${col}`);
+  displayArray();
 }
 
 //changing cells from alive to dead
@@ -53,17 +70,141 @@ function clickcells(td) {
     }
 }
 
-
-
-//boolean to make the game run
-var isrunning = false;
-
-//buttons
-function start() {
-    if (isrunning != false) {
-        isrunning = true;
+function createArray(){
+  //I'm using the rows and columns variable to make it easier to understand the structure of my array
+  let rows= size;
+  let columns=size;
+  gameArray=[];
+  for(let i=0;i<rows; i++){
+    //Initialize a new column
+    gameArray[i]=[];
+    inactive_array[i]=[];
+    for(let j=0;j<columns;j++){
+      gameArray[i][j]=0;
+      inactive_array[i][j]=0;
     }
+  }
+  //inactive_array=gameArray;
+//  for (let i = 0; i < size; i++) {
+ //               for (let j = 0; j < size; j++) {
+  //                inactive_array[i][j]= gameArray[i][j];
+                  
+    //            }
+   //}
 }
-function stop() {
-    isrunning = false;
+function displayArray(){
+  for(let i=0;i<size; i++){
+  
+    for(let j=0;j<size;j++){
+      let id= "td_" + i + "_" + j;
+      let curr=document.getElementById(id);
+      if(gameArray[i][j]==0){
+        curr.classList.add('dead');
+        curr.classList.remove('alive');
+      }
+      if(gameArray[i][j]==1){
+        curr.classList.add('alive');
+        curr.classList.remove('dead');
+      }
+    }
+  }
 }
+
+function start(){
+  //Start a timer to increment a generation every 5 sec
+  //Use an interval ?
+}
+function stop(){
+  //Use clearinterval to stop that timer
+}
+function reset(){
+  for(let i=0;i<size; i++){
+    //Initialize a new column
+    gameArray[i]=[];
+    for(let j=0;j<size;j++){
+      gameArray[i][j]=0;
+    }
+  }
+  displayArray();
+}
+//Game of life functions
+function updateLifeCycle() {
+
+            for (let i = 0; i < size; i++) {
+                for (let j = 0; j < size; j++) {
+                  
+                  console.log(`Examining ${i} , ${j}`);
+                  console.log(gameArray.toString());
+                    let new_state = updateCellValue(i, j);
+                    inactive_array[i][j] = new_state;
+                }
+            }
+   for (let i = 0; i < size; i++) {
+                for (let j = 0; j < size; j++) {
+                  gameArray[i][j]= inactive_array[i][j];
+                  
+                }
+   }
+            //gameArray = inactive_array;
+         //   document.getElementById("generation").innerHTML = this.generation;
+}
+function gen1(){
+  console.log(gameArray.toString());
+  updateLifeCycle();
+  displayArray();
+}
+function gen23(){
+  updateLifeCycle();
+  displayArray();
+}
+function updateCellValue(row, col){
+
+            let total = countNeighbours(row, col);
+            if(gameArray[row][col]== 1){
+              //We are looking at a live cell
+              if(total<2 || total>3){
+                //If it's underpopulated or overcrowded
+                return 0;
+              }else return 1;//Else it lives
+              
+            }
+            if(gameArray[row][col]==0){
+              //We are looking at a dead cell
+              if(total==3){
+                //If it has 3 neighbors it becomes alive
+                return 1;
+              }else return 0;
+              
+            }
+}
+
+function countNeighbours(row, col){
+            let total_neighbours = 0;
+            total_neighbours += setCellValueHelper(row - 1, col - 1);
+            total_neighbours += setCellValueHelper(row - 1, col);
+            total_neighbours += setCellValueHelper(row - 1, col + 1);
+            total_neighbours += setCellValueHelper(row, col - 1);
+            total_neighbours += setCellValueHelper(row, col + 1);
+            total_neighbours += setCellValueHelper(row + 1, col - 1);
+            total_neighbours += setCellValueHelper(row + 1, col);
+            total_neighbours += setCellValueHelper(row + 1, col + 1);
+            console.log("Total Neighbors:"+total_neighbours);
+            return total_neighbours;
+}
+function setCellValueHelper(row, col){
+  console.log(`trying [${row}][${col}]`);
+  console.log(gameArray.toString());
+            if(row<0||row>=size){
+              console.log("Row exceeded size");
+              return 0;              
+            }else if(col<0||col>=size){
+              console.log("Column exceeded size");
+              return 0;            
+            }else {
+              console.log("Valid input:"+gameArray[row][col]);
+              return gameArray[row][col];
+            }
+            
+}
+
+
